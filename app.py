87766cae5eca765879f049139abc2c3877eccf3f
Ricardo Furbino <ricardo@newagesports.net>
2023-05-17 19:28:12 -0300

    feat: implements shrinkerer

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import random
+import string
 
 app = Flask(__name__)
+
+def generate_short_link():
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(6))
+
+@app.route('/shorten', methods=['POST'])
+def shorten_link():
+    original_url = request.json['url']
+    short_link = generate_short_link()
+    return jsonify({'short_link': short_link})
 
 if __name__ == '__main__':
     app.run()
