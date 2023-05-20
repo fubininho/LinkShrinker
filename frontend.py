7ec068d7aff2b843245dd1fbeee3b63ab808bb3c
Thiago Miarelli <thiagomiarelli@gmail.com>
2023-05-20 15:12:25 -0300

    Revert "Thiago"

--- frontend.py
+++ frontend.py
@@ -14,6 +14,7 @@ def shorten_url(url):
     link = Link(url, short_link)
     return link
 
+
 def get_links():
     response = requests.get(f'{BACKEND_URL}/links')
     data = response.json()
@@ -23,46 +24,42 @@ def get_links():
         links.append(link)
     return links
 
-def render_shorten_input():
+
+st.title('Link Shortener')
+
+option = st.selectbox('Choose an action:', ('Shorten URL', 'View Shortened Links'))
+
+if option == 'Shorten URL':
     url = st.text_input('Enter the URL to shorten:')
     if st.button('Shorten'):
         if url:
             link = shorten_url(url)
             shortened_link = f'{BACKEND_URL}/{link.shortened_link}'
-            st.success(f'Shortened URL: [{shortened_link}](http://{link.shortened_link})', icon='🤩')
+            st.success(f'Shortened URL: [{shortened_link}](http://{link.original_link})')
         else:
             st.warning('Please enter a URL.')
 
-def delete_link(shortened_link):
-    requests.delete(f'{BACKEND_URL}/links?shortened_link={shortened_link}')
-
-
-def render_link_table():
+elif option == 'View Shortened Links':
     links = get_links()
+    selected_link = None  # Initialize selected_link as None
     if links:
-        data = []
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            st.write(f'Encurtado')
-        with col2:
-            st.write(f'Original')
-        with col3:
-            st.write(f'Ações')
-        for link in links:
-            col1, col2, col3 = st.columns(3)
-            with col1:
-                st.write(f'[short.ly/{link.shortened_link}]({BACKEND_URL}/{link.shortened_link})')
-            with col2:
-                st.write(f'[http://{link.original_link}](http://{link.original_link})')
-            with col3:
-                 if st.button("delete", key=link.shortened_link):
-                    delete_link(link.shortened_link)
+        data = [[f'<a href="{BACKEND_URL}/{link.shortened_link}" target="_blank">{BACKEND_URL}/{link.shortened_link}</a>',
+                 link.original_link] for link in links]
+        df = pd.DataFrame(data, columns=['Shortened Link', 'Original Link'])
+        st.write('Shortened Links:')
+        st.write(df.to_html(escape=False, index=False), unsafe_allow_html=True)
+        delete_clicked = st.button("Select a link to be Deleted")
+        if delete_clicked:
+            selected_link = st.selectbox("Select a link to delete", links, format_func=lambda link: link.shortened_link)
+            if selected_link:
+                delete_response = requests.delete(f'{BACKEND_URL}/links?shortened_link={selected_link.shortened_link}')
+                if delete_response.status_code == 200:
+                    st.success(f"Link {selected_link.shortened_link} deleted successfully.")
+                    links.remove(selected_link)
+                    selected_link = None  # Reset selected_link to None after successful deletion
                     st.experimental_rerun()
+                else:
+                    st.error(f"Failed to delete link {selected_link.shortened_link}.")
+    else:
+        st.write('No shortened links available.')
 
-    
-
-
-
-st.title('Link Shortener')
-render_shorten_input()
-render_link_table()
